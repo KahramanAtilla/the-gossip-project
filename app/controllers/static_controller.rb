b49dcee209c4da162d67041fbbd42351d2e188d1
Atilla <atillafacebook@hotmail.fr>
2019-10-28 16:26:03 +0100

    potin et author en base de donnes via post dans lurl gossipnew

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,4 +10,17 @@ class StaticController < ApplicationController
 
   def new_gossip
   end
+
+  def new_gossip_post
+   puts "Hello depuis le serveur"
+   a = Author.create
+   a.first_name = params["first_name"]
+   a.last_name = params["last_name"]
+   a.save
+   u = Potin.create(author: a)
+   u.title = params["title"]
+   u.content = params["content"]
+   u.save
+   puts "C'est fini"
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
   get '/gossipnew', to: 'static#new_gossip'
+  post '/gossipnew', to: 'static#new_gossip_post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
